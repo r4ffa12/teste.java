20d1e0b7c402a0acc87261fffb0cc11f025bad3f
Rafael Santos Braulio <104738238+r4ffa12@users.noreply.github.com>
2024-03-19 19:01:11 -0300

    Add files via upload

---
+++ calculo.java
@@ -0,0 +1,14 @@
+public class calculo {
+    public static void main(String[] args){
+        int INDICE = 13;
+        int SOMA = 0;
+        int K = 0;
+
+        while (K < INDICE) {
+            K = K + 1;
+            SOMA = SOMA + K;
+            
+        }
+        System.out.println(SOMA);
+    }
+}
